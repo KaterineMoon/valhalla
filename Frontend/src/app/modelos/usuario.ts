8b560677d6323230aa90ea5dae6c55eb2ed61bef
laura.camelo <laura.camelo.33@gmail.com>
2019-11-08 17:29:48 -0500

    Modificacion en el componente de afiliacion

---
+++ Frontend/src/app/modelos/usuario.ts
@@ -0,0 +1,10 @@
+export class Usuario {
+
+    constructor(
+        public _id:string,
+        public nombre:string,
+        public edad: Number,
+        public correo:string,
+        public password:string,
+    ){}
+}
