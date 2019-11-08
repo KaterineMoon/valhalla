e8156645ba539d830514e98f969693b09f7eb6ef
Diana Muñoz <diana.munoz@fitideas.co>
2019-11-07 23:35:02 -0500

    new components generated

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -9,15 +9,23 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 
-import { ComponentsModule } from './components/components.module';
+import { ComponentsModule } from './componentes/components.module';
 import { ExamplesModule } from './examples/examples.module';
+import { MainContentComponent } from './componentes/main-content/main-content.component';
+import { CesantiasComponent } from './componentes/cesantias/cesantias.component';
+import { InversionComponent } from './componentes/inversion/inversion.component';
+import { PensionComponent } from './componentes/pension/pension.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    MainContentComponent,
+    CesantiasComponent,
+    InversionComponent,
+    PensionComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/src/app/componentes/registro-empleado/registro-empleado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-empleado',
+  templateUrl: './registro-empleado.component.html',
+  styleUrls: ['./registro-empleado.component.scss']
+})
+export class RegistroEmpleadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
