294fc56048fb6219bf2a806a704ca74274adb5b8
jlvalbuenac <programacion.jlvalbuenac@gmail.com>
2019-11-15 21:24:26 -0500

    Perfil empresas

--- Frontend/src/app/componentes/informacion-empresa/informacion-empresa.component.ts
+++ Frontend/src/app/componentes/informacion-empresa/informacion-empresa.component.ts
@@ -1,4 +1,6 @@
+import { Empresa } from './../../modelos/empresa';
 import { Component, OnInit } from '@angular/core';
+import { HttpClientService } from '../../servicios/http-client.service';
 
 @Component({
   selector: 'app-informacion-empresa',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./informacion-empresa.component.css']
 })
 export class InformacionEmpresaComponent implements OnInit {
+  empresa : Empresa;
+  url = "http://localhost:3977/api/";
 
-  constructor() { }
+  constructor(
+    private _httpClientService: HttpClientService
+  ) { }
 
   ngOnInit() {
+    this.empresa = JSON.parse(localStorage.getItem("sesion"))
+    console.log(this.empresa)
   }
 
 }
--- Frontend/src/app/componentes/inversion/inversion.component.ts
+++ Frontend/src/app/componentes/inversion/inversion.component.ts
@@ -13,3 +13,20 @@ export class InversionComponent implements OnInit {
   }
 
 }
+
+
+  
+  
+  
+
+  
+
+  
+
+ 
+
+
+   
+
+  
+ 
