f65bfe3f6a3451dae775607483b43dcdd341751f
Diana Muñoz <diana.munoz@fitideas.co>
2019-11-15 19:07:54 -0500

    carousel in inversiones

--- Frontend/src/app/componentes/inversion/inversion.component.ts
+++ Frontend/src/app/componentes/inversion/inversion.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-inversion',
   templateUrl: './inversion.component.html',
-  styleUrls: ['./inversion.component.css']
+  styleUrls: ['./inversion.component.css'],
+  providers: [NgbCarouselConfig]
 })
 export class InversionComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 5000;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+  }  
 
   ngOnInit() {
   }
--- Frontend/src/app/componentes/main-content/main-content.component.ts
+++ Frontend/src/app/componentes/main-content/main-content.component.ts
@@ -12,8 +12,6 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 export class MainContentComponent implements OnInit {
 
-  images = [700, 533, 807, 124].map((n) => `https://picsum.photos/id/${n}/900/500`);
-
   constructor(config: NgbCarouselConfig) {
     // customize default values of carousels used by this component tree
     config.interval = 5000;
